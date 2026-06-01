872a98812bb0105164b3569c32681f97bde1de79
sadek135 <57948772+sadek135@users.noreply.github.com>
2026-06-01 11:23:29 +0300

    fixed camera preview after using the arrow to go back only

--- src/screens/RegisterFacesScreen.js
+++ src/screens/RegisterFacesScreen.js
@@ -149,7 +149,7 @@ const RegisterFacesScreen = ({ navigation }) => {
       <StatusBar barStyle="light-content" />
 
       <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation?.goBack()}>
+        <TouchableOpacity onPress={() => navigation?.navigate("Home")}>
           <ArrowLeft size={28} color="#FFFFFF" />
         </TouchableOpacity>
         <Text style={styles.headerTitle}>{t.title}</Text>
